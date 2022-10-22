753f97a0a7d6b9c1c3ec5f94b49b1b469c2efa22
octaprog7 <goctaprog@gmail.com>
2022-10-22 23:09:04 +0500

    Initial commit

---
+++ ina_ti.py
@@ -0,0 +1,128 @@
+"""INAxxx Texas Instruments sensors module"""
+
+# micropython
+# MIT license
+# Copyright (c) 2022 Roman Shevchik   goctaprog@gmail.com
+
+from sensor_pack import bus_service
+from sensor_pack.base_sensor import BaseSensor, Iterator, check_value
+# from sensor_pack import bitfield
+# import struct
+# import array
+
+
+class INA219(BaseSensor, Iterator):
+    """Class for work with TI INA219 sensor"""
+    def __init__(self, adapter: bus_service.BusAdapter, address=0x40):
+        """shunt_resistance - сопротивление шунта, [Ом]"""
+        super().__init__(adapter, address, True)    # All data bytes are transmitted most significant byte first.
+        self.bus_voltage_range = True  # False - 16 V; True - 32 V
+        self.continuous_mode = True    # непрерывный или однократный режимы работы
+        self.bus_voltage = True        # измерение напряжения на шине, режимы работы
+        self.shunt_voltage = True      # измерение тока, путем измерения напряжения на шунте, режимы работы
+        # value     range, mV
+        # 0         ±40 mV
+        # 1         ±80 mV
+        # 2         ±160 mV
+        # 3         ±320 mV     default
+        self._current_shunt_voltage_range = 3   # диапазон напряжения, измеряемого на токовом шунте      0..3
+        # делаю "недоступным" для пользователя. это лишние настройки!
+        # разрешение/усреднение АЦП напряж. на токовом шунте. 12 bit. Conversion time: 532 μs
+        self._current_adc_resolution = 3
+        # разрешение/усреднение АЦП входного изменяемого напряж. 12 bit. Conversion time: 532 μs
+        self._voltage_adc_resolution = 3
+
+    def _set_raw_cfg(self, value: int) -> int:
+        return self._write_register(0x00, value, 2)
+
+    def _get_raw_cfg(self) -> int:
+        b = self._read_register(0x00, 2)
+        return self.unpack("H", b)[0]
+
+    @property
+    def current_shunt_voltage_range(self):
+        return self._current_shunt_voltage_range
+
+    @current_shunt_voltage_range.setter
+    def current_shunt_voltage_range(self, value):
+        self._current_shunt_voltage_range = check_value(value, range(4),
+                                                        f"Invalid current shunt voltage range: {value}")
+
+    def set_config(self) -> int:
+        """Настраивает датчик в соответствии с настройками"""
+        raw_cfg = int(self.bus_voltage_range) << 13     # Bus Voltage Range bit
+        raw_cfg |= self._current_shunt_voltage_range << 11   # PGA (Shunt Voltage Only)
+        raw_cfg |= self._voltage_adc_resolution << 7         # BADC Bus ADC Resolution/Averaging
+        raw_cfg |= self._current_adc_resolution << 3         # SADC Shunt ADC Resolution/Averaging
+        # mode
+        raw_cfg |= int(self.continuous_mode) << 2   # continuous
+        raw_cfg |= int(self.bus_voltage) << 1   # Bus voltage
+        raw_cfg |= int(self.shunt_voltage)      # Shunt voltage
+        #
+        return self._set_raw_cfg(raw_cfg)
+
+    def get_config(self) -> int:
+        """Считывает настройками датчика по шине"""
+        raw_cfg = self._get_raw_cfg()
+        # mode
+        self.shunt_voltage = bool(raw_cfg & 0x0001)     # измерять напряжение на токовом шунте
+        self.bus_voltage = bool(raw_cfg & 0x0002)       # измерять напряжение на шине
+        self.continuous_mode = bool(raw_cfg & 0x0004)   # непрерывный режим измерений
+        #
+        self._current_adc_resolution = (raw_cfg & 0b0000_0000_0111_1000) >> 3
+        self._voltage_adc_resolution = (raw_cfg & 0b0000_0111_1000_0000) >> 7
+        self._current_shunt_voltage_range = (raw_cfg & 0b0000_1000_0000_0000) >> 11
+        # диапазон входного измеряемого напряжения: False-16 V; True - 32 V
+        self.bus_voltage_range = bool(raw_cfg & 0b0010_0000_0000_0000)
+        #
+        return raw_cfg
+
+    # BaseSensor
+    def _read_register(self, reg_addr, bytes_count=2) -> bytes:
+        """считывает из регистра датчика значение.
+        bytes_count - размер значения в байтах"""
+        b = self.adapter.read_register(self.address, reg_addr, bytes_count)
+        return b
+
+        # BaseSensor
+    def _write_register(self, reg_addr, value: [int, bytes, bytearray], bytes_count=2) -> int:
+        """записывает данные value в датчик, по адресу reg_addr.
+        bytes_count - кол-во записываемых данных"""
+        byte_order = self._get_byteorder_as_str()[0]
+        return self.adapter.write_register(self.address, reg_addr, value, bytes_count, byte_order)
+
+    def get_shunt_voltage(self) -> float:
+        """Возвращает напряжение на шунте в Вольтах"""
+        # DC ACCURACY:  ADC basic resolution: 12 bit;    Shunt voltage, 1 LSB step size: 10 μV
+        reg_raw = self._read_register(0x01, 2)
+        return 1E-5 * self.unpack("h", reg_raw)[0]
+
+    def get_voltage(self) -> tuple:
+        """Возвращает входное измеряемое напряжение, флаг готовности данных, флаг математического переполнения (OVF).
+        Флаг математического переполнения (OVF) устанавливается, когда расчеты мощности или тока выходят за допустимые
+        пределы. Это указывает на то, что данные о токе и мощности могут быть бессмысленными!"""
+        # DC ACCURACY:  ADC basic resolution: 12 bit;    Bus voltage, 1 LSB step size: 4 mV
+        reg_raw = self._read_register(0x02, 2)[0]
+        ovf = bool(reg_raw & 0x01)
+        cnvr = bool(reg_raw & 0x02)
+        return 0.004 * (reg_raw >> 2), cnvr, ovf
+
+    def get_power(self):
+        reg_raw = self._read_register(0x03, 2)[0]
+        ...
+
+    def get_current(self):
+        reg_raw = self._read_register(0x04, 2)[0]
+        ...
+
+    def get_id(self):
+        return None
+
+    def soft_reset(self):
+        self._set_raw_cfg(0b11100110011111)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        raise NotImplementedError
---
+++ main.py
@@ -0,0 +1,34 @@
+import utime
+from machine import I2C
+from sensor_pack.bus_service import I2cAdapter
+import ina_ti
+
+
+if __name__ == '__main__':
+    # пожалуйста установите выводы scl и sda в конструкторе для вашей платы, иначе ничего не заработает!
+    # please set scl and sda pins for your board, otherwise nothing will work!
+    # https://docs.micropython.org/en/latest/library/machine.I2C.html#machine-i2c
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) # для примера
+    # bus =  I2C(scl=Pin(4), sda=Pin(5), freq=100000)   # на esp8266    !
+    # Внимание!!!
+    # Замените id=1 на id=0, если пользуетесь первым портом I2C !!!
+    # Warning!!!
+    # Replace id=1 with id=0 if you are using the first I2C port !!!
+    i2c = I2C(id=1, freq=400_000)  # on Arduino Nano RP2040 Connect tested
+    adaptor = I2cAdapter(i2c)
+    # bme - sensor
+    ina219 = ina_ti.INA219(adaptor)
+    conf = ina219.get_config()
+    print(f"chip raw config: {hex(conf)}")
+    print(f"chip config:")
+    print(f"\tcontinuous mode: {ina219.continuous_mode}")
+    print(f"\tshunt voltage: {ina219.shunt_voltage}")
+    print(f"\tbus voltage: {ina219.bus_voltage}")
+    #
+    print(f"bus voltage range: {32 if ina219.bus_voltage_range else 16} V")
+
+    while True:
+        shunt_v = ina219.get_shunt_voltage()
+        t = ina219.get_voltage()
+        print(f"Shunt voltage: {shunt_v} V; Bus voltage: {t[0]} V; data ready flag: {t[1]}; overflow flag: {t[2]}")
+        utime.sleep_ms(1000)
